0caf8f7b703a90c8affbc6e01ae53c2901de4645
ErikBatista <erikbatista42@gmail.com>
2018-07-12 11:52:35 -0700

    Implemented the Photo Source Selection Popover

--- Makestagram/Helpers/MGPhotoHelper.swift
+++ Makestagram/Helpers/MGPhotoHelper.swift
@@ -14,5 +14,28 @@ class MGPhotoHelper: NSObject {
    
     func presentActionSheet(from viewController: UIViewController) {
         
+        let alertController = UIAlertController(title: nil, message: "Where do you want to get your picture from?", preferredStyle: .actionSheet)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let capturePhotoAction = UIAlertAction(title: "Take Photo", style: .default, handler: { action in
+                
+            })
+                
+                
+                alertController.addAction(capturePhotoAction)
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let uploadAction = UIAlertAction(title: "Upload from Library", style: .default, handler: { action in
+                
+            })
+            
+                alertController.addAction(uploadAction)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+            viewController.present(alertController, animated: true)
     }
 }
