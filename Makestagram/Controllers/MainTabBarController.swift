2c357dfa07f9e8ef0e8d2c780ccd3d96ee4f298c
ErikBatista <erikbatista42@gmail.com>
2018-07-11 19:59:23 -0700

    added mgphotohelper

---
+++ Makestagram/Controllers/MainTabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  MainTabBarController.swift
+//  Makestagram
+//
+//  Created by Erik Batista on 7/11/18.
+//  Copyright © 2018 Erik Batista. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+        tabBar.unselectedItemTintColor = .black
+    }
+}
+
+extension MainTabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController.tabBarItem.tag == 1 {
+            // present photo taking action sheet
+            print("take photo")
+            return false
+        } else {
+            return true
+        }
+    }
+}
---
+++ Makestagram/Helpers/MGPhotoHelper.swift
@@ -0,0 +1,18 @@
+//
+//  MGPhotoHelper.swift
+//  Makestagram
+//
+//  Created by Erik Batista on 7/11/18.
+//  Copyright © 2018 Erik Batista. All rights reserved.
+//
+
+import UIKit
+
+class MGPhotoHelper: NSObject {
+
+    var completionHandler: ((UIImage) -> Void)?
+   
+    func presentActionSheet(from viewController: UIViewController) {
+        
+    }
+}
