c5c6cc7604d99ca4722bc3efd197a45119f85bdc
ErikBatista <erikbatista42@gmail.com>
2018-07-12 11:59:43 -0700

    connected the MGPhotoHelper with the MainTabBarController

--- Makestagram/Controllers/MainTabBarController.swift
+++ Makestagram/Controllers/MainTabBarController.swift
@@ -11,8 +11,15 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
     
+    let photoHelper = MGPhotoHelper()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        photoHelper.completionHandler = { image in
+            print("handle image")
+        }
+        
         delegate = self
         tabBar.unselectedItemTintColor = .black
     }
@@ -21,11 +28,10 @@ class MainTabBarController: UITabBarController {
 extension MainTabBarController: UITabBarControllerDelegate {
     func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
         if viewController.tabBarItem.tag == 1 {
-            // present photo taking action sheet
-            print("take photo")
+            photoHelper.presentActionSheet(from: self)
             return false
-        } else {
-            return true
         }
+        
+        return true
     }
 }
