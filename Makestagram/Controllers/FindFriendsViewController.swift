5c0d8efb9278260e8bfa7863928222e1c09ef908
ErikBatista <erikbatista42@gmail.com>
2018-07-11 19:16:07 -0700

    connected all tab bars to view controllers

---
+++ Makestagram/Controllers/FindFriendsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FindFriendsViewController.swift
+//  Makestagram
+//
+//  Created by Erik Batista on 7/11/18.
+//  Copyright © 2018 Erik Batista. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FindFriendsViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
---
+++ Makestagram/Controllers/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Makestagram
+//
+//  Created by Erik Batista on 7/11/18.
+//  Copyright © 2018 Erik Batista. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
